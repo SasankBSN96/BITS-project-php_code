fb66f68d88210a7001dc890ce0cc09b932871d17
Shashank Bonthala <sivasasank39@gmail.com>
2017-03-13 22:11:28 +0530

    commented the code

--- upload.php
+++ upload.php
@@ -1,9 +1,12 @@
 <?php
      
-        $file_path = "H:\uploads/";
-        //print_r($_FILES);
-		//print_r($_POST);
+        //Path to the directory where videos get saved
+		$file_path = "H:\uploads/"; 
+
+		//Appending the name of the video file to the file path
         $file_path = $file_path . basename( $_FILES['video']['name']);
+		
+		//Save the video to the directory and check if the upload was successful
         if(move_uploaded_file($_FILES['video']['tmp_name'], $file_path)) {
             echo "success";
         } else{
