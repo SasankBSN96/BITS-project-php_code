c58708f7831694d28f0643c4de2664ac9d7cfc4f
Shashank Bonthala <sivasasank39@gmail.com>
2017-03-13 22:22:44 +0530

    change indentation

--- upload.php
+++ upload.php
@@ -3,10 +3,10 @@
         //Path to the directory where videos get saved
 		$file_path = "H:\uploads/"; 
 
-		//Appending the name of the video file to the file path
+	//Appending the name of the video file to the file path
         $file_path = $file_path . basename( $_FILES['video']['name']);
 		
-		//Save the video to the directory and check if the upload was successful
+	//Save the video to the directory and check if the upload was successful
         if(move_uploaded_file($_FILES['video']['tmp_name'], $file_path)) {
             echo "success";
         } else{
